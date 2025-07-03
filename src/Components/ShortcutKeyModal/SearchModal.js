55d799f1cba765d6412c08375625b0ecb1a86d5b
Pikalot <pikalot@hotmail.com>
2025-07-03 12:52:46 -0700

    Used scoped CSS

--- src/Components/ShortcutKeyModal/SearchModal.js
+++ src/Components/ShortcutKeyModal/SearchModal.js
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState, useCallback, useMemo } from 'react';
-import style from './SearchModal.module.css';
+// import style from './SearchModal.module.css';
 import { officerOrAdminRoutes, signedOutRoutes, memberRoutes, notAuthenticatedRoutes } from '../../Routes';
 import { membershipState } from '../../Enums';
 import { useUser } from '../context/UserContext';
@@ -28,6 +28,32 @@ export default function SearchModal({ appProps }) {
   const handleChanges = (e) => {
     setKeyword(e.target.value);
     setSelectItem(0);
+  }
+
+  function getSuggestions() {
+    if (suggestions.length === 0) return <></>;
+
+    return (
+          <ul className='suggestion-list'>
+            {suggestions.map((r, index) => (
+              <li
+                key={index}
+                className={`'suggestion-item' ${index === selectItem ? 'active' : ''}`}
+                onMouseEnter={() => setSelectItem(index)}
+                onClick={() => {
+                  window.location.href = r.path;
+                  setOpen(false);
+                }}
+              >
+                <span style={{ marginRight: '0.5rem' }}>
+                  {r.type === 'user' ? '👤' : '📄'}
+                </span>
+                {r.pageName}
+                <div className='hidden-tab'>{selectItem === index && `${window.location.origin}${r.path}`}</div>
+              </li>
+            )).slice(0, 5)}
+          </ul>
+    )
   }
 
   /**
@@ -131,7 +157,7 @@ export default function SearchModal({ appProps }) {
   if (!open) return null;
 
   return (
-    <div className='modal'>
+    <div className='search-modal'>
       <div className='input-wrapper'>
         <input
           ref={inputRef}
@@ -139,7 +165,9 @@ export default function SearchModal({ appProps }) {
           value={keyword}
           onChange={handleChanges} />
 
-        {suggestions.length > 0 && (
+        {getSuggestions()}
+
+        {/* {suggestions.length > 0 && (
           <ul className={`${style['suggestion-list']}`}>
             {suggestions.map((r, index) => (
               <li
@@ -159,7 +187,7 @@ export default function SearchModal({ appProps }) {
               </li>
             )).slice(0, 5)}
           </ul>
-        )}
+        )} */}
       </div>
       <div>
         {errorMsg && <p>{errorMsg}</p>}
