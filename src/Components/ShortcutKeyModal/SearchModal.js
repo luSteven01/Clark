a30243693a4a524a9caf2857ff0c5c637687c1f6
Pikalot <pikalot@hotmail.com>
2025-07-08 23:14:55 -0700

    Merged with dev's code

--- src/Components/ShortcutKeyModal/SearchModal.js
+++ src/Components/ShortcutKeyModal/SearchModal.js
@@ -4,6 +4,7 @@ import { officerOrAdminRoutes, signedOutRoutes, memberRoutes, notAuthenticatedRo
 import { membershipState } from '../../Enums';
 import { getAllUsers } from '../../APIFunctions/User';
 import { useUser } from '../context/UserContext';
+import { useAuth } from '../context/AuthContext';
 
 export default function SearchModal({ appProps }) {
   const [open, setOpen] = useState(false);
@@ -14,10 +15,11 @@ export default function SearchModal({ appProps }) {
   const [selectItem, setSelectItem] = useState(0);
   const { user } = useUser();
   const [errorMsg, setErrorMsg] = useState('');
+  const { authenticated } = useAuth();
 
   /**
    * Returns the appropriate routes array based on the user's access level.
-   * @dependencies user.accessLevel, appProps.authenticated
+   * @dependencies user.accessLevel, authenticated
    */
   const routes = useMemo(() => {
     if (user.accessLevel === membershipState.MEMBER)
@@ -30,13 +32,13 @@ export default function SearchModal({ appProps }) {
         ...officerOrAdminRoutes.filter(r => r.pageName !== 'Edit User Info'),
         ...signedOutRoutes
       ];
-    if (!appProps.authenticated)
+    if (!authenticated)
       return [
         ...notAuthenticatedRoutes,
         ...signedOutRoutes
       ];
     return [...signedOutRoutes];
-  }, [user.accessLevel, appProps.authenticated]);
+  }, [user.accessLevel, authenticated]);
 
   /**
    * Helper function updates the keyword when the user types
